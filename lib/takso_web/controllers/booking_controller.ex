16cec3981e1b9ce901a56921e16325061f9ee065
Zhaosi Qu <git@chaos.run>
2021-10-29 21:51:44 +0300

    [TDD - Pass] reject same addresses

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -27,33 +27,41 @@ defmodule TaksoWeb.BookingController do
 
   def create(conn, booking_params) do
     user = conn.assigns.current_user
+    pickup_address = booking_params["pickup_address"]
+    dropoff_address = booking_params["dropoff_address"]
 
-    booking_struct = Ecto.build_assoc(user, :bookings, Enum.map(booking_params, fn({key, value}) -> {String.to_atom(key), value} end))
-    changeset = Booking.changeset(booking_struct, %{})
-                |> Changeset.put_change(:status, "open")
+    if pickup_address == dropoff_address do
+      conn
+      |> put_flash(:error, "Addresses can not be the same!")
+      |> redirect(to: Routes.booking_path(conn, :index))
+    else
+      booking_struct = Ecto.build_assoc(user, :bookings, Enum.map(booking_params, fn({key, value}) -> {String.to_atom(key), value} end))
+      changeset = Booking.changeset(booking_struct, %{})
+                  |> Changeset.put_change(:status, "open")
 
-    booking = Repo.insert!(changeset)
+      booking = Repo.insert!(changeset)
 
-    query = from t in Taxi, where: t.status == "available", select: t
-    available_taxis = Repo.all(query)
+      query = from t in Taxi, where: t.status == "available", select: t
+      available_taxis = Repo.all(query)
 
-    case length(available_taxis) > 0 do
-      true -> taxi = List.first(available_taxis)
-              Multi.new
-              |> Multi.insert(:allocation, Allocation.changeset(%Allocation{}, %{status: "accepted"}) |> Changeset.put_change(:booking_id, booking.id) |> Changeset.put_change(:taxi_id, taxi.id))
-              |> Multi.update(:taxi, Taxi.changeset(taxi, %{}) |> Changeset.put_change(:status, "busy"))
-              |> Multi.update(:booking, Booking.changeset(booking, %{}) |> Changeset.put_change(:status, "allocated"))
-              |> Repo.transaction
+      case length(available_taxis) > 0 do
+        true -> taxi = List.first(available_taxis)
+                Multi.new
+                |> Multi.insert(:allocation, Allocation.changeset(%Allocation{}, %{status: "accepted"}) |> Changeset.put_change(:booking_id, booking.id) |> Changeset.put_change(:taxi_id, taxi.id))
+                |> Multi.update(:taxi, Taxi.changeset(taxi, %{}) |> Changeset.put_change(:status, "busy"))
+                |> Multi.update(:booking, Booking.changeset(booking, %{}) |> Changeset.put_change(:status, "allocated"))
+                |> Repo.transaction
 
-              conn
-              |> put_flash(:info, "Your taxi will arrive in 5 minutes")
-              |> redirect(to: Routes.booking_path(conn, :index))
-      _    -> Booking.changeset(booking) |> Changeset.put_change(:status, "rejected")
-              |> Repo.update
+                conn
+                |> put_flash(:info, "Your taxi will arrive in 5 minutes")
+                |> redirect(to: Routes.booking_path(conn, :index))
+        _    -> Booking.changeset(booking) |> Changeset.put_change(:status, "rejected")
+                |> Repo.update
 
-              conn
-              |> put_flash(:info, "At present, there is no taxi available!")
-              |> redirect(to: Routes.booking_path(conn, :index))
+                conn
+                |> put_flash(:info, "At present, there is no taxi available!")
+                |> redirect(to: Routes.booking_path(conn, :index))
+      end
     end
   end
 
