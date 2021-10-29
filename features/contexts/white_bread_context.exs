ce43fc2c48aa7768c49f201b41f0434565a89ecf
Zhaosi Qu <git@chaos.run>
2021-10-29 19:55:00 +0300

    [BDD] fix a welcome message test step

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -18,7 +18,7 @@ defmodule WhiteBreadContext do
 
   scenario_finalize fn _status, _state ->
     Ecto.Adapters.SQL.Sandbox.checkin(Takso.Repo)
-    Hound.end_session
+    # Hound.end_session
   end
 
   given_ ~r/^the following taxis are on duty$/, fn state, %{table_data: table} ->
@@ -50,6 +50,7 @@ defmodule WhiteBreadContext do
   end
 
   then_ ~r/^I should receive a welcome message$/, fn state ->
+    assert visible_in_page? ~r/Welcome test@example.com/
     {:ok, state}
   end
 
