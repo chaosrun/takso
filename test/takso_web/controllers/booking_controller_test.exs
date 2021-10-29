5c6a1e7c0d2567c5de5ac206562864d4208cd6ba
Zhaosi Qu <git@chaos.run>
2021-10-29 22:35:24 +0300

    [TDD - Fail] test address can not be empty

--- test/takso_web/controllers/booking_controller_test.exs
+++ test/takso_web/controllers/booking_controller_test.exs
@@ -29,6 +29,26 @@ defmodule TaksoWeb.BookingControllerTest do
     assert html_response(conn, 200) =~ ~r/Addresses can not be the same!/
   end
 
+  test "Empty pickup address", %{conn: conn} do
+    conn = post conn, "sessions", %{session: [username: "test@example.com", password: "12345678"]}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Welcome Tester/
+    Repo.insert!(%Taxi{status: "available"})
+    conn = post conn, "bookings", %{pickup_address: "", dropoff_address: "Liivi 2"}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Address can not be empty!/
+  end
+
+  test "Empty dropoff address", %{conn: conn} do
+    conn = post conn, "sessions", %{session: [username: "test@example.com", password: "12345678"]}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Welcome Tester/
+    Repo.insert!(%Taxi{status: "available"})
+    conn = post conn, "bookings", %{pickup_address: "Liivi 2", dropoff_address: ""}
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Address can not be empty!/
+  end
+
   test "Booking Rejection", %{conn: conn} do
     conn = post conn, "sessions", %{session: [username: "test@example.com", password: "12345678"]}
     conn = get conn, redirected_to(conn)
