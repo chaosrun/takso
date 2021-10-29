eefb4124e2f9e73e7acee9aebf7169fbc92b57a0
Zhaosi Qu <git@chaos.run>
2021-10-29 22:40:37 +0300

    [TDD - Pass] test address can not be empty

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -37,32 +37,43 @@ defmodule TaksoWeb.BookingController do
     else
       booking_struct = Ecto.build_assoc(user, :bookings, Enum.map(booking_params, fn({key, value}) -> {String.to_atom(key), value} end))
       changeset = Booking.changeset(booking_struct, %{})
-                  |> Changeset.put_change(:status, "open")
+                  |> Changeset.put_change(:status, "OPEN")
+      distance = get_distance(pickup_address, dropoff_address)
 
-      booking = Repo.insert!(changeset)
+      case Repo.insert(changeset) do
+        {:ok, booking} ->
+          query = from t in Taxi, where: t.status == "available", select: t
+          available_taxis = Repo.all(query)
+          case length(available_taxis) > 0 do
+            true -> taxi = List.first(available_taxis)
+                    Multi.new
+                    |> Multi.insert(:allocation, Allocation.changeset(%Allocation{}, %{status: "ALLOCATED"}) |> Changeset.put_change(:booking_id, booking.id) |> Changeset.put_change(:taxi_id, taxi.id))
+                    |> Multi.update(:taxi, Taxi.changeset(taxi, %{}) |> Changeset.put_change(:status, "BUSY"))
+                    |> Multi.update(:booking, Booking.changeset(booking, %{}) |> Changeset.put_change(:status, "allocated"))
+                    |> Repo.transaction
 
-      query = from t in Taxi, where: t.status == "available", select: t
-      available_taxis = Repo.all(query)
+                    conn
+                    |> put_flash(:info, "Your taxi will arrive in 5 minutes")
+                    |> redirect(to: Routes.booking_path(conn, :index))
+            _    -> Booking.changeset(booking) |> Changeset.put_change(:status, "REJECTED")
+                    |> Repo.update
 
-      case length(available_taxis) > 0 do
-        true -> taxi = List.first(available_taxis)
-                Multi.new
-                |> Multi.insert(:allocation, Allocation.changeset(%Allocation{}, %{status: "accepted"}) |> Changeset.put_change(:booking_id, booking.id) |> Changeset.put_change(:taxi_id, taxi.id))
-                |> Multi.update(:taxi, Taxi.changeset(taxi, %{}) |> Changeset.put_change(:status, "busy"))
-                |> Multi.update(:booking, Booking.changeset(booking, %{}) |> Changeset.put_change(:status, "allocated"))
-                |> Repo.transaction
-
-                conn
-                |> put_flash(:info, "Your taxi will arrive in 5 minutes")
-                |> redirect(to: Routes.booking_path(conn, :index))
-        _    -> Booking.changeset(booking) |> Changeset.put_change(:status, "rejected")
-                |> Repo.update
-
-                conn
-                |> put_flash(:info, "At present, there is no taxi available!")
-                |> redirect(to: Routes.booking_path(conn, :index))
+                    conn
+                    |> put_flash(:info, "At present, there is no taxi available!")
+                    |> redirect(to: Routes.booking_path(conn, :index))
+          end
+        {:error, _changeset} ->
+          conn
+          |> put_flash(:error, "Address can not be empty!")
+          |> redirect(to: Routes.booking_path(conn, :new))
       end
+
     end
+
+  end
+
+  def get_distance(_pickup_address, _dropoff_address) do
+    5
   end
 
 end
