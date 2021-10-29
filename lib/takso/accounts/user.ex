e3329041382f30363bc3447e401e2c72cf5b8d23
Zhaosi Qu <git@chaos.run>
2021-10-29 21:13:29 +0300

    fix bug

--- lib/takso/accounts/user.ex
+++ lib/takso/accounts/user.ex
@@ -19,7 +19,7 @@ defmodule Takso.Accounts.User do
     |> cast(params, [:name, :username, :password, :age])
     |> validate_required([:name, :username, :password, :age])
     |> unique_constraint(:username)
-    |> validate_format(:email, ~r/@/)
+    |> validate_format(:username, ~r/@/)
     |> validate_number(:age, greater_than_or_equal_to: 18)
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,13 +12,13 @@
 
 alias Takso.{Repo, Accounts.User, Sales.Taxi}
 
-[%{name: "Fred Flintstone", username: "fred", password: "parool"},
- %{name: "Barney Rubble", username: "barney", password: "parool"},
- %{name: "Tester", username: "test@example.com", password: "12345678"}]
+[%{name: "Fred Flintstone", username: "fred@example.com", password: "parool", age: 19},
+ %{name: "Barney Rubble", username: "barney@example.com", password: "parool", age: 20},
+ %{name: "Tester", username: "test@example.com", password: "12345678", age: 21}]
 |> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
 
-[%{username: "driver_1", location: "Narva 25", status: "busy", user_id: 1},
- %{username: "driver_2", location: "Liivi 2", status: "busy", user_id: 2}]
+[%{username: "fred@example.com", location: "Narva 25", status: "busy", user_id: 1, capacity: 4, price: 1.8},
+ %{username: "barney@example.com", location: "Liivi 2", status: "busy", user_id: 2, capacity: 3, price: 1.2}]
 |> Enum.map(fn taxi_data -> Taxi.changeset(%Taxi{}, taxi_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
